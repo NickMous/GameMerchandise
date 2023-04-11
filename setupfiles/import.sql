cbbaa1d20ab3d4b2fc97c23d0a7f11cf57969d65
Sander den Hollander <sanderdenhollander12@gmail.com>
2023-04-11 10:18:18 +0200

    'updated import'

--- setupfiles/import.sql
+++ setupfiles/import.sql
@@ -12,35 +12,115 @@ CREATE TABLE `gamemerch` (
   `foto` varchar(255) DEFAULT NULL
 );
 
-INSERT INTO `gamemerch` (`naam`, `game`, `prijs`, `voorraad`, `id`, `beschrijving`, `foto`) VALUES
-('Pickaxe', 'Fortnite', 9.99, 5, 1, 'Fortnite Pickaxe', 'pickaxe.jpg'),
-('Backpack', 'Fortnite', 14.99, 3, 2, 'Fortnite Backpack', 'backpack.jpg'),
-('Creeper', 'Minecraft', 4.99, 15, 3, 'Minecraft Creeper', 'creeper.png'),
-('ai', 'ai', 2.99, 17, 9, 'geen idee', 'badtoy.jpg');
+INSERT INTO
+  `gamemerch` (
+    `naam`,
+    `game`,
+    `prijs`,
+    `voorraad`,
+    `id`,
+    `beschrijving`,
+    `foto`
+  )
+VALUES
+  (
+    'Pickaxe',
+    'Fortnite',
+    9.99,
+    5,
+    1,
+    'Fortnite Pickaxe',
+    'pickaxe.jpg'
+  ),
+  (
+    'Backpack',
+    'Fortnite',
+    14.99,
+    3,
+    2,
+    'Fortnite Backpack',
+    'backpack.jpg'
+  ),
+  (
+    'Creeper',
+    'Minecraft',
+    4.99,
+    15,
+    3,
+    'Minecraft Creeper',
+    'creeper.png'
+  ),
+  (
+    'Toy plushie',
+    'Toy Story',
+    5.99,
+    17,
+    'Just a random ass toy',
+    'badtoy.jpg'
+  ),
+  (
+    'struisvogel',
+    'Struisvogel simulator',
+    19.99,
+    98,
+    'Jeroen de struisvogel',
+    'jeroen.jpg'
+  );
+
+(
+  'ai',
+  'ai',
+  2.99,
+  17,
+  9,
+  'geen idee',
+  'badtoy.jpg'
+);
 
 CREATE TABLE `userdata` (
   `username` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `id` int NOT NULL,
-  `admin` enum('no','yes') NOT NULL DEFAULT 'no',
+  `admin` enum('no', 'yes') NOT NULL DEFAULT 'no',
   `pfp` varchar(100) DEFAULT NULL
 );
 
-INSERT INTO `userdata` (`username`, `password`, `id`, `admin`, `pfp`) VALUES
-('sanderei', '$2y$10$KtxiLXjL.fmuLmBOLfG4u.YRwJy50ohHNpXLV/yHRgHypT2Is293e', 24, 'yes', 'badtoy.jpg'),
-('testuser', '$2y$10$3nakWazf9NgpoeUC9/J6vOG2pRA0VLFS8i00uDPsoytooA8KuKV9.', 26, 'no', NULL);
+INSERT INTO
+  `userdata` (`username`, `password`, `id`, `admin`, `pfp`)
+VALUES
+  (
+    'sanderei',
+    '$2y$10$KtxiLXjL.fmuLmBOLfG4u.YRwJy50ohHNpXLV/yHRgHypT2Is293e',
+    24,
+    'yes',
+    'badtoy.jpg'
+  ),
+  (
+    'testuser',
+    '$2y$10$3nakWazf9NgpoeUC9/J6vOG2pRA0VLFS8i00uDPsoytooA8KuKV9.',
+    26,
+    'no',
+    NULL
+  );
 
+ALTER TABLE
+  `gamemerch`
+ADD
+  PRIMARY KEY (`id`);
 
-ALTER TABLE `gamemerch`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE
+  `userdata`
+ADD
+  PRIMARY KEY (`id`);
 
-ALTER TABLE `userdata`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE
+  `gamemerch`
+MODIFY
+  `id` int NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 10;
 
-
-ALTER TABLE `gamemerch`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
-ALTER TABLE `userdata`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
-
+ALTER TABLE
+  `userdata`
+MODIFY
+  `id` int NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 27;
